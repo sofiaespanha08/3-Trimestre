c68c10aad7925314982d9a278bcc09510dc960be
sofiaespanha08 <129747191+sofiaespanha08@users.noreply.github.com>
2024-10-21 11:37:50 -0300

    Add files via upload

--- common.js
+++ common.js
@@ -1,12 +1,5 @@
 const getCSS = (variavel) => {
-    const bodyStyles = getComputedStyle(document.body)
-    return bodyStyles.getPropertyValue(variavel)
+    return getComputedStyle(document.body).getPropertyValue(variavel)
 }
 
-const tickConfig = {
-    family: getCSS('--font'),
-    size: 16,
-    color: getCSS('--primary-color')
-}
-
-export {getCSS, tickConfig}
+export {getCSS}
--- quantidadeUsuarios.js
+++ quantidadeUsuarios.js
@@ -1,4 +1,4 @@
-import { getCSS, tickConfig } from "./common.js"
+import { getCSS } from "./common.js"
 
 async function quantidadeUsuariosPorRede() {
     const url = 'https://raw.githubusercontent.com/guilhermeonrails/api/main/numero-usuarios.json'
@@ -18,42 +18,16 @@ async function quantidadeUsuariosPorRede() {
         }
     ]
 
-    const laytout = {
+    const layout = {
         plot_bgcolor: getCSS('--bg-color'),
-        paper_bgcolor: getCSS('--bg-color'),
-        title: {
-            text: 'Redes sociais com mais usuários',
-            x: 0,
-            font: {
-                color: getCSS('--primary-color'),
-                size: 30,
-                font: getCSS('--font')
-            }
-        },
-        xaxis: {
-            tickfont: tickConfig,
-            title: {
-                text: 'Nome das redes',
-                font: {
-                    color: getCSS('--secondary-color')
-                }
-            }
-        },
-        yaxis: {
-            tickfont: tickConfig,
-            title: {
-                text: 'Bilhões de usuários ativos',
-                font: {
-                    color: getCSS('--secondary-color'
-                }
-            }
-        }
+        paper_bgcolor: getCSS('--bg-color')
+
     }
 
     const grafico = document.createElement('div')
     grafico.className = 'grafico'
     document.getElementById('graficos-container').appendChild(grafico)
-    Plotly.newPlot(grafico, data, laytout)
+    Plotly.newPlot(grafico, data, layout)
 }
 
 quantidadeUsuariosPorRede()
